4ae8a1815b1ba77d1e47cac93ed9faafddaa1671
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-16 15:31:04 +0200

    feat: implements about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
+
+const About: NextPage = () => (
+    <Layout>
+      <main><h1>About test</h1></main>
+    </Layout>
+  )
+
+export default About
