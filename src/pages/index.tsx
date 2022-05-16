304d2930bd76c39429a7120ed09d6c8148d27a82
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-16 15:28:50 +0200

    feat: refactor index.tsx to implement Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,10 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
 
 const Home: NextPage = () => (
-    <div className={styles.container}>
-      <Head>
-        <title>gBoole01</title>
-        <meta name="description" content="gBoole01 Blog" />
-        {/* TODO => Add favicon logo */}
-        <link rel="icon" href="/favicon.ico" /> 
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to gBoole01 Blog
-        </h1>
-      </main>
-
-      <footer className={styles.footer}>
-      </footer>
-    </div>
+    <Layout>
+      <main><h1>Test</h1></main>
+    </Layout>
   )
 
 export default Home
