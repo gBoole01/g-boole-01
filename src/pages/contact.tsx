57537024fdb8b6df4eedf755a5cddd5c19faf9d1
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-16 15:32:14 +0200

    feat: implement contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
+
+const Contact: NextPage = () => (
+    <Layout>
+      <main><h1>Contact test</h1></main>
+    </Layout>
+  )
+
+export default Contact
