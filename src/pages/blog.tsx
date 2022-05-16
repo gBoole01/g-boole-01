3028df03c8ecaa728cd04c1f7be12d3333803849
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-16 15:31:23 +0200

    feat: implement blog page

---
+++ src/pages/blog.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
+
+const Blog: NextPage = () => (
+    <Layout>
+      <main><h1>Blog test</h1></main>
+    </Layout>
+  )
+
+export default Blog
