1ccf8a42d697b757655bab855d08fcfeeba25345
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-16 15:32:40 +0200

    feat: implement resume page

---
+++ src/pages/resume.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
+
+const Resume: NextPage = () => (
+    <Layout>
+      <main><h1>Resume test</h1></main>
+    </Layout>
+  )
+
+export default Resume
