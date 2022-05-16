84d839bf0054184a3a86b4756c388f6fa93cda9a
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-16 15:33:09 +0200

    feat: implement layout component

---
+++ src/components/layout.tsx
@@ -0,0 +1,37 @@
+import Head from "next/head";
+import Header from "./header";
+import Footer from "./footer";
+
+export default function Layout({
+    children
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                <title>gBoole01</title>
+                <meta 
+                    name="description"
+                    content="gBoole01 Website"
+                />
+                <meta 
+                    property="og:image"
+                    content="/favicon.ico"
+                />
+                <meta 
+                    name="og-title"
+                    content="gBoole01 Website"
+                />
+                <meta 
+                    name="twitter:card"
+                    content="summary_large_image"
+                />
+            </Head>
+            <Header />
+            <main>{children}</main>
+            <Footer />
+        </>
+    )
+}
