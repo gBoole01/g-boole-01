f98b28d4a55a4cdbd41f84136d9af4bdee7d72da
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-16 15:33:24 +0200

    feat: implement header component

---
+++ src/components/header.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Test Header</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/blog">
+                            <a>Blog</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/resume">
+                            <a>Resume</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a>About</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">
+                            <a>Contact</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
