58b70d1619b82db4fd5cdc924f9ec178e0981e18
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-17 19:08:36 +0200

    feat: implement project colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      'blue-1': '#0072b1',
+      'blue-2': '#4cbffc',
+      'gray-1': '#404040',
+      'gray-2': '#666666',
+      'gray-3': '#8c8c8c',
+      'white': '#FFFFFF'
+    },
+    fontFamily: {
+      sans: ['Noto Sans', 'sans-serif'],
+      serif: ['Georgia', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
