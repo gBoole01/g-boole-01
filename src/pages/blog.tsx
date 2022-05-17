f4f0c604bd23b784f9cb39693e306799417d82ef
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-17 19:15:32 +0200

    feat: start blog integration

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import Layout from '../components/layout';
 
 const Blog: NextPage = () => (
     <Layout>
-      <main><h1>Blog test</h1></main>
+      <h1>Blog test</h1>
+      <Link href="/blog/article">
+        <a>Article 1</a>
+      </Link>
     </Layout>
   )
 
