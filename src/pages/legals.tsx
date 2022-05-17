a9b1b56c9113626580de72b56696f131783adf5e
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-17 19:07:27 +0200

    feat: implement legals page

---
+++ src/pages/legals.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next';
+import Layout from '../components/layout';
+
+const Legals: NextPage = () => (
+    <Layout>
+      <h1>Mentions Légales</h1>
+    </Layout>
+  )
+
+export default Legals
