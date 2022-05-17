7dbb30c97775c578b7ce9443f751a8712509234e
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-17 19:09:08 +0200

    feat: start footer integration

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,62 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 export default function Footer() {
-    return <footer>Test Footer</footer>
+    return (
+        <footer className="border-t-4">
+            <nav>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <a>Accueil</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/blog">
+                            <a>Blog</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about">
+                            <a>A propos</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact">
+                            <a>Contact</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/terms-and-conditions">
+                            <a>CGU</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/legals">
+                            <a>Mentions Légales</a>
+                        </Link>
+                    </li>
+
+                </ul>
+            </nav>
+            <div className="text-center p-y-2 bg-gray-1">
+                <small className="text-white">
+                    &copy; Copyright 2022 gBoole01 
+                </small>
+                <a 
+                    className="ml-2"
+                    href="https://github.com/gBoole01"
+                    target="_blank"
+                    >
+                    <Image
+                        priority
+                        src="/images/github.png"
+                        height={15}
+                        width={15}
+                        alt="Github Logo"                  
+                        />
+                </a>
+            </div>
+        </footer>
+    )
 }
