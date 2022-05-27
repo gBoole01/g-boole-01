78c4637e6a7c70747bfe9b6a3ccebf5f66dce4d5
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-27 22:21:20 +0200

    feat: start header integration

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,33 +3,53 @@ import Image from 'next/image';
 
 export default function Header() {
     return (
-        <header className="flex border-b-4">
-            <Link href="/">
-                <a>
-                    <Image
-                        priority
-                        src="/images/logo-512.png"
-                        height={150}
-                        width={150}
-                        alt="Website Logo"
-                    />
-                </a>
-            </Link>
+        <header className="
+            relative
+            after:content-['']
+            after:absolute
+            after:right-20
+            after:top-5
+            after:bg-blue-2
+            after:w-px
+            after:h-20
+            before:content-['']
+            before:absolute
+            before:right-10
+            before:bottom-0
+            before:bg-blue-2
+            before:w-20
+            before:h-px
+        ">
             <nav>
-                <ul>
-                    <li>
-                        <Link href="/blog">
-                            <a>Blog</a>
+                <ul className="pt-5 px-9 flex items-center justify-between after:content-[''] after:absolute after:left-1/2 after:bg-blue-2 after:w-0.5 after:h-12">
+                    <li className="w-1/5">
+                        <Link href="/">
+                            <a>
+                                <Image
+                                    priority
+                                    src="/images/logo-512.png"
+                                    height={64}
+                                    width={64}
+                                    alt="Website Logo"
+                                />
+                            </a>
                         </Link>
                     </li>
-                    <li>
-                        <Link href="/about">
-                            <a>A propos</a>
-                        </Link>
-                    </li>
-                    <li>
+                    <div className="w-3/5 grid grid-cols-2 gap-5">
+                        <li className="text-right">
+                            <Link href="/blog">
+                                <a>Blog</a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/about">
+                                <a>A Propos</a>
+                            </Link>
+                        </li>
+                    </div>
+                    <li className="w-1/5 text-right">
                         <Link href="/contact">
-                            <a>Contact</a>
+                            <a>Mail</a>
                         </Link>
                     </li>
                 </ul>
