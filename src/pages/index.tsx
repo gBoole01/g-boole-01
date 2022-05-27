e682edbd94a2611318acf66d713dbc8c1c57ef16
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-27 22:23:20 +0200

    feat: change h1 color

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from '../components/layout';
 
 const Home: NextPage = () => (
     <Layout>
-      <h1>Accueil</h1>
+      <h1 className="text-white">gBoole01</h1>
     </Layout>
   )
 
