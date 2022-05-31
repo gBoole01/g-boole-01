4305d6c8f94f07c0edb942a12db0c2ab2287a4ad
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-05-31 17:20:30 +0200

    feat: integrate page title

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from 'next';
+import Head from 'next/head'; 
 import Layout from '../components/layout';
 
 const Contact: NextPage = () => (
     <Layout>
+      <Head>
+        <title>Contact | gBoole01</title>
+      </Head>
+
       <h1>Contact</h1>
     </Layout>
   )
