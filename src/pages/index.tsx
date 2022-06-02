71d72e657d89b5a9be9d5d9cdb5d012c07547ed7
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-02 18:17:21 +0200

    feat: integrate HeroPost component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,73 +2,71 @@ import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
 import Layout from '../components/layout';
+import Container from '../components/container';
 import { getLastNPosts } from '../lib/getPosts';
 import PostType from '../types/Post';
+import HeroPost from '../components/hero-post';
+import { SITENAME } from '../lib/constants';
 
 type Props = {
-  heroPost: PostType;
   allPosts: PostType[];
 }
 
 
-export default function Home({ heroPost, allPosts }: Props) {
+export default function Home({ allPosts }: Props) {
+  const heroPost = allPosts[0];
+  const otherPosts = allPosts.slice(1);
+
   return (
     <Layout>
       <Head>
-        <title>Accueil | gBoole01</title>
+        <title>Accueil | { SITENAME }</title>
       </Head>
 
-      <h1>Accueil</h1>
-      <h2>{ heroPost.title }</h2>
-      <p>Le { heroPost.date } - {heroPost.duration } min de lecture</p>
-      <Image
-        priority
-        src={`/images/blog/${heroPost.image}`}
-        width={213}
-        height={120}
-      />
-      <p>{ heroPost.content }</p>
-      <Link href={`/blog/${heroPost.slug}`}>
-        <a>
-          Lire la suite
-          <Image
-            src="/images/arrow-right.svg"
-            height={20}
-            width={20}
+      <Container>
+        <h1 className="text-white">Accueil</h1>
+        {heroPost && (
+          <HeroPost
+          title={heroPost.title}
+          date={heroPost.date}
+          duration={heroPost.duration}
+          image={heroPost.image}
+          excerpt={heroPost.content}
+          slug={heroPost.slug}
           />
-        </a>
-      </Link>
-      <ul>
-        {allPosts.map(({ slug, content, date, duration, image, title }) => (
-          <li key={slug}>
-            <h2>{ title }</h2>
-            <p>Le { date } - { duration } min de lecture</p>
-            <Image
-              src={`/images/blog/${image}`}
-              width={90}
-              height={90}
-            />
-            <p>{ content }</p>
-            <Link href={`/blog/${slug}`}>
-              <a>
-                Lire la suite
-                <Image
-                  src="/images/arrow-right.svg"
-                  height={20}
-                  width={20}
+          )}
+        <ul>
+          {otherPosts.map(({ slug, content, date, duration, image, title }) => (
+            <li key={slug}>
+              <h2>{ title }</h2>
+              <p>Le { date } - { duration } min de lecture</p>
+              <Image
+                src={`/images/blog/${image}`}
+                width={90}
+                height={90}
                 />
-              </a>
+              <p>{ content }</p>
+              <Link href={`/blog/${slug}`}>
+                <a>
+                  Lire la suite
+                  <Image
+                    src="/images/arrow-right.svg"
+                    height={20}
+                    width={20}
+                    />
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <h2>Qui suis-je ?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis p</p>
+        <button>
+          <Link href="/about">
+              <a>En savoir plus</a>
             </Link>
-          </li>
-        ))}
-      </ul>
-      <h2>Qui suis-je ?</h2>
-      <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis p</p>
-      <button>
-        <Link href="/about">
-            <a>En savoir plus</a>
-          </Link>
-      </button>
+        </button>
+      </Container>
     </Layout>
   );
 }
@@ -83,11 +81,8 @@ export const getStaticProps = async () => {
     'image'
   ]);
 
-  const heroPost = allPosts.shift();
-
   return {
     props: {
-      heroPost,
       allPosts
     }
   };
