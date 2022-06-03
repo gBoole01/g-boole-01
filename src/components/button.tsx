93f67f3ce9224932a25fe0d54e159bc3d3620981
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-03 19:59:45 +0200

    feat: implement Button component

---
+++ src/components/button.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+type Props = {
+    label: string;
+    href: string;
+}
+
+const Button = ({
+    label,
+    href,
+}: Props) => (
+        <Link href={href}>
+            <button className="bg-blue-1 border border-blue-2 text-blue-2 text-xs font-bold rounded-xl py-px px-4">{label}</button>
+        </Link>
+)
+
+
+export default Button;
