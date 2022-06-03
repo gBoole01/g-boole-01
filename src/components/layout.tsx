cd86a04285ffdd471241765cadbeee2e5f84187a
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-03 21:38:05 +0200

    feat: integrate Meta component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,54 +1,20 @@
-import Head from "next/head";
-import Header from "./header";
-import Footer from "./footer";
+import Header from './header';
+import Footer from './footer';
+import Meta from './meta';
 
-export default function Layout({
+type Props = {
+    children? : React.ReactNode;
+}
+
+const Layout = ({ 
     children
-}: {
-    children: React.ReactNode;
-}) {
-    return (
+}: Props) => (
         <div className="container mx-auto font-sans">
-            <Head>
-                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-                <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-                <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-                <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-                <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-                <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-                <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-                <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-                <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-                <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-                <link rel="manifest" href="/manifest.json"/>
-                <meta name="msapplication-TileColor" content="#ffffff"/>
-                <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
-                <meta name="theme-color" content="#ffffff"/>  
-                <title>gBoole01</title>
-                <meta 
-                    name="description"
-                    content="gBoole01 Website"
-                />
-                <meta 
-                    property="og:image"
-                    content="/favicon.ico"
-                />
-                <meta 
-                    name="og-title"
-                    content="gBoole01 Website"
-                />
-                <meta 
-                    name="twitter:card"
-                    content="summary_large_image"
-                />
-            </Head>
+            <Meta />
             <Header />
-            <main>{children}</main>
+            <main className="px-9">{children}</main>
             <Footer />
         </div>
     )
-}
+
+export default Layout;
