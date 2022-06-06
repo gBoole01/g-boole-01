8150763f260d6858709d648bd1fcf9d595c36133
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-06 17:41:34 +0200

    feat: add spacing above section and between list items

--- src/components/resume-experiences.tsx
+++ src/components/resume-experiences.tsx
@@ -1,12 +1,12 @@
 import tw from 'tailwind-styled-components';
 
-const Section = tw.section`before:absolute before:bg-blue-1 before:bottom-0 before:content-[''] before:left-[-0.625rem] before:top-10 before:w-[3px] relative`
+const Section = tw.section`before:absolute before:bg-blue-1 before:bottom-0 before:content-[''] before:left-[-0.625rem] before:top-10 before:w-[3px] mt-4 relative`
 const SectionTitle = tw.h2`text-lg text-blue-1 font-bold italic uppercase`
 const Title = tw.h3`before:absolute before:bg-blue-1 before:bottom-1/4 before:content-[''] before:h-4 before:left-[-1rem] before:rounded-full before:top-1/4 before:w-4 relative pl-1 text-lg uppercase`
 const Subtitle = tw.p`text-lg text-blue-2 italic`
 const DateCaption = tw.span`text-sm text-blue-1`
 const List = tw.ul`list-['-'] pl-2`
-const ListItem = tw.li`pl-1`
+const ListItem = tw.li`pl-1 pb-1`
 
 const ResumeExperiences = () => (
     <Section>
@@ -39,9 +39,9 @@ const ResumeExperiences = () => (
         </Title>
         <Subtitle>Etablissement de restauration rapide</Subtitle>
         <List>
-          <ListItem>Assurer l'ouverture et la fermeture de l'établissement</ListItem>
           <ListItem>Former, motiver et superviser une équipe</ListItem>
           <ListItem>Assurer la qualité des produits et services</ListItem>
+          <ListItem>Assurer l'ouverture et la fermeture de l'établissement</ListItem>
         </List>
     </Section>
 )
