faa01bfb78d8a7d29c493f18c9a7bda1cb9a4507
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-07 17:39:32 +0200

    feat: implement handler() for contact-form API

---
+++ src/pages/api/contact-form.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { SITENAME } from '../../lib/constants';
+import getMailTransport from '../../lib/getMailTransport';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { body } = req;
+    const { name, email, message } = body;
+
+    if (!name) { res.status(401).json({ data: 'No name provided'}) }
+    if (!email) { res.status(401).json({ data: 'No email provided'}) }
+    if (!message) { res.status(401).json({ data: 'No message provided'}) }
+
+    const transporter = getMailTransport();
+    const response = await transporter.sendMail({
+        from: `"${name}" <${email}>`,
+        to: process.env.SMTP_CONTACT_MAILBOX,
+        subject: `Demande de contact depuis ${SITENAME}`,
+        text: message,
+        html: `<div>${message}</div>`,
+        headers: {}
+    });
+
+    if (!response) { res.status(500).json({ data: 'Something went wrong while sending message'}) }
+
+    res.status(200).json({ data: 'OK'});
+}
+
+export default handler;
