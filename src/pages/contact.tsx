7185e87c15b96a864c9fd64bfe34b7098e1b67f9
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-09 00:28:05 +0200

    feat: integrate contact form; implement validation; improve error handling

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -8,34 +8,74 @@ const Contact: NextPage = () => {
     const [email, setEmail] = useState('')
     const [message, setMessage] = useState('')
 
-    const [submitted, setSubmitted] = useState(false)
-    const [error, setError] = useState(false)
-    const [errorMessage, setErrorMessage] = useState('')
+    const [errors, setErrors] = useState({
+        name: false,
+        email: false,
+        message: false,
+    })
+
+    const [buttonText, setButtonText] = useState('Envoyer')
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+    const handleValidation = () => {
+        const tempErrors = {
+            name: false,
+            email: false,
+            message: false,
+        };
+        let isValid = true;
+
+        if (name.length <= 0) {
+            tempErrors.name = true;
+            isValid = false;
+        }
+
+        if (email.length <= 0) {
+            tempErrors.email = true;
+            isValid = false;
+        }
+
+        if (message.length <= 0) {
+            tempErrors.message = true;
+            isValid = false;
+        }
+
+        setErrors({ ...tempErrors });
+        return isValid;
+    }
 
     const handleSubmit = async (event: SyntheticEvent) => {
         event.preventDefault();
-        const data = {
-            name,
-            email,
-            message,
-        }
+        setShowSuccessMessage(false);
+        setShowFailureMessage(false);
+        const isValidForm = handleValidation();
 
-        const response = await fetch('/api/contact-form', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-        });
+        if (isValidForm) {
+            setButtonText('Envoi en cours...');
+            
+            const response = await fetch('/api/contact-form', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    message,
+                }),
+            });
 
-        setSubmitted(true);
-        const result = await response.json();
-        if (response.status !== 200) {
-            setError(true);
-            setErrorMessage(result.data);
-        } else {
-            setError(false);
-            setErrorMessage('');
+            const { error } = await response.json();
+            if (error) {
+                setShowSuccessMessage(false);
+                setShowFailureMessage(true);
+                setButtonText('Envoyer');
+                return;
+            }
+            setShowSuccessMessage(true);
+            setShowFailureMessage(false);
+            setButtonText('Envoyer');
             setName('');
             setEmail('');
             setMessage('');
@@ -51,29 +91,71 @@ const Contact: NextPage = () => {
 
             <h1 className="mt-6 mb-4 text-2xl text-blue-1 font-bold uppercase">Contact</h1>
 
-            {submitted && error && (<p>{errorMessage}</p>)}
-
-            {submitted && !error && (
-                <>
-                    <p>Votre message a bien été envoyé !</p>
-                    <p>Je vous apporterai une réponse dès que possible</p>
-                </>
-            )}
-
             <section>
-                <form className="flex flex-col gap-2 items-center mt-6" onSubmit={handleSubmit}>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" onChange={(e) => { setName(e.target.value) }} value={name} className="border p-1" />
+                <p>Si vous souhaitez échanger sur n'importe quel sujet, vous pouvez m'envoyer un message avec le formulaire suivant.</p>
+                <p>N'oubliez pas de renseigner une adresse e-mail valide afin que je puisse vous apporter une réponse !</p>
+                <form className="flex flex-col gap-2 mt-6" onSubmit={handleSubmit}>
+                    <label className="flex flex-col" htmlFor="name">
+                        <span>Votre Nom <span className="text-red font-bold">*</span></span>
+                        <input 
+                            type="text" 
+                            id="name"
+                            name="name"
+                            placeholder="George Boole"
+                            onChange={(e) => { setName(e.target.value) }}
+                            value={name}
+                            className="mt-2 p-2 border border-gray-3 rounded-md shadow-sm focus:border-blue-2 focus:ring focus:ring-blue-2 focus:ring-opacity-50" 
+                        />
+                        {errors?.name && (<p className="text-red italic">Le nom ne peut pas être vide</p>)}
+                    </label>
 
-                    <label htmlFor="email">E-mail</label>
-                    <input type="email" id="email" name="email" onChange={(e) => { setEmail(e.target.value) }} value={email} className="border p-1" />
+                    <label className="flex flex-col" htmlFor="email">
+                        <span>Votre E-mail <span className="text-red font-bold">*</span></span>
+                        <input 
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="george.boole@exemple.fr"
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            value={email}
+                            className="mt-2 p-2 border border-gray-3 rounded-md shadow-sm focus:border-blue-2 focus:ring focus:ring-blue-2 focus:ring-opacity-50" 
+                        />
+                        {errors?.email && (<p className="text-red italic">L'adresse mail ne peut pas être vide</p>)}
+                    </label>
 
-                    <label htmlFor="message">Message</label>
-                    <textarea name="message" id="message" onChange={(e) => { setMessage(e.target.value) }} value={message} className="border w-full p-2" />
+                    <label className="flex flex-col" htmlFor="message">
+                        <span>Votre Message <span className="text-red font-bold">*</span></span>
+                        <textarea 
+                            name="message"
+                            id="message"
+                            rows={4}
+                            placeholder="De quoi voulez-vous parler ?"
+                            onChange={(e) => { setMessage(e.target.value) }}
+                            value={message}
+                            className="mt-2 p-2 w-full border border-gray-3 rounded-md shadow-sm resize-none focus:border-blue-2 focus:ring focus:ring-blue-2 focus:ring-opacity-50" 
+                        />
+                        {errors?.message && (<p className="text-red italic">Le message ne peut pas être vide</p>)}
+                    </label>
 
-                    <button type="submit">Envoyer</button>
+                    <input 
+                        type="submit"
+                        value={buttonText}
+                        className="p-2 w-full border-2 border-blue-2 text-white font-bold bg-blue-1 rounded-md shadow-md focus:border-blue-2 focus:ring focus:ring-blue-2 focus:ring-opacity-50" 
+                    />
                 </form>
             </section>
+
+            {showSuccessMessage && (
+                <p className="text-blue-1 font-bold">
+                    Votre message a bien été envoyé !
+                </p>
+            )}
+
+            {showFailureMessage && (
+                <p className="text-red font-bold">
+                    Un problème est survenu lors de l'envoi de votre message..
+                </p>
+            )}
 
         </Layout>
     )
