4c9c4579ee0790f31efd298852fd8de60b1ce71f
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-08 16:17:24 +0200

    feat: implement SEO in Meta component

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -1,44 +1,115 @@
 import Head from 'next/head';
-import { SITENAME } from '../lib/constants';
+import { useRouter } from 'next/router';
+import { SITE_PROPS } from '../lib/constants';
+import Favicon from './favicon';
 
-const Meta = () =>  (
-    <Head>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/manifest.json"/>
-        <meta name="msapplication-TileColor" content="#ffffff"/>
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
-        <meta name="theme-color" content="#ffffff"/>  
-        <meta 
-            name="description"
-            content="gBoole01 Website"
-        />
-        <meta 
-            property="og:image"
-            content="/favicon.ico"
-        />
-        <meta 
-            name="og-title"
-            content={SITENAME}
-        />
-        <meta 
-            name="twitter:card"
-            content="summary_large_image"
-        />
-        <title>{SITENAME}</title>
-    </Head>
-)
+type Props = {
+    title: string;
+    description?: string;
+    canonical?: string;
+    post?: {
+        image: string;
+        publicationDate: string;
+        modificationDate: string;
+    }
+}
+
+const Meta = ({
+    title,
+    description,
+    canonical,
+    post,
+}: Props) => {
+    const router = useRouter();
+
+    return (
+        <Head>
+            <Favicon />
+            <meta charSet="UTF-8" key="charset" />
+            <meta name="viewport" content="width=device-width,initial-scale=1" key="viewport" />
+            {canonical && (
+                <link rel="canonical" href={canonical} key="canonical" />
+            )}
+            <meta property="og:locale" content={SITE_PROPS.locale} key="og:locale" />
+            <meta property="og:site_name" content={SITE_PROPS.siteName} key="og:sitename" />
+            
+            <title>{`${title} | ${SITE_PROPS.siteName}`}</title>
+            <meta 
+                property="og:title"
+                content={`${title} | ${SITE_PROPS.siteName}`}
+                key="og:title"
+            />
+            <meta 
+                name="description"
+                content={ description || SITE_PROPS.description }
+                key="description"
+            />
+            <meta 
+                property="og:description"
+                content={ description || SITE_PROPS.description }
+                key="og:description"
+            />
+            <meta name="author" content={SITE_PROPS.author} key="author" />
+
+            {post && (
+                <>
+                    <meta property="og:type" content="article" key="og:type" />
+                    <meta 
+                        property="og:image"
+                        content={`${SITE_PROPS.url}/images/blog/${post.image}`}
+                        key="og:image"
+                    />
+                    <meta 
+                        name="twitter:card"
+                        content="summary_large_image"
+                        key="twitter:card"
+                    />
+                    <meta 
+                        property="article:published_time"
+                        content={new Date(post.publicationDate).toISOString()}
+                        key="article:published_time"
+                    />
+                    <meta 
+                        property="article:modified_time"
+                        content={new Date(post.modificationDate).toISOString()}
+                        key="article:modified_time"
+                    />
+                    <script
+                        type="application/ld+json"
+                        dangerouslySetInnerHTML={{
+                            __html: `{
+                                "description": "${description || SITE_PROPS.description}",
+                                "author": {
+                                    "@type": "Person",
+                                    "name": "${SITE_PROPS.author}"
+                                },
+                                "@type": "BlogPosting",
+                                "url": "${SITE_PROPS.url}/",
+                                "publisher": {
+                                    "@type": "Person",
+                                    "name": "${SITE_PROPS.author}",
+                                    "logo": {
+                                        "@type": "ImageObject",
+                                        "url": "${SITE_PROPS.url}/images/logo-512.png",
+                                    }
+                                },
+                                "headline": "${title} | ${SITE_PROPS.siteName}",
+                                "image": ["${SITE_PROPS.url}/images/blog/${post.image}"],
+                                "datePublished": "${new Date(post.publicationDate).toISOString()}",
+                                "dateModified": "${new Date(post.modificationDate).toISOString()}",
+                                "mainEntityOfPage": {
+                                    "@type": "WebPage",
+                                    "@id": "${SITE_PROPS.url}${router.asPath}",
+                                },
+                                "@context": "http://schema.org"
+                            }`
+                        }}
+                        key="ldjson"
+                    />
+                </>
+            )}
+        </Head>
+    )
+}
 
 export default Meta;
