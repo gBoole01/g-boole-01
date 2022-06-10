15911453db71db1614ce586ad351169f96ad9dae
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2022-06-10 13:26:58 +0200

    feat: import opquastLogo rather than dynamic import

--- src/components/resume-diplomas.tsx
+++ src/components/resume-diplomas.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image';
 import tw from 'tailwind-styled-components';
+import Image from 'next/image';
+import opquastLogo from '../../public/images/opquast-expert-badge.png'
 
-const Section = tw.section`relative after:absolute after:z-[-1] after:content[''] after:bg-blue-2/20 after:rounded-l-lg after:left-0 after:right-[-2.25rem] after:top-0 after:bottom-0 py-2 mt-6`
+const Section = tw.section`relative after:absolute after:z-[-1] after:content[''] after:bg-blue-2/20 after:rounded-l-lg after:left-[-0.25rem] after:right-[-2.25rem] after:top-0 after:bottom-0 py-2 mt-6`
 const SectionTitle = tw.h2`text-lg text-blue-1 font-bold italic uppercase`
 const Title = tw.h3`pl-1 text-lg uppercase`
 const Subtitle = tw.p`text-lg text-blue-2 italic`
@@ -28,7 +29,7 @@ const ResumeDiplomas = () => (
             <Subtitle>Opquast - Niveau Expert</Subtitle>
             <Image
                 priority
-                src="/images/opquast-expert-badge.png"
+                src={opquastLogo}
                 height={76}
                 width={100}
                 alt="opquast expert badge"
