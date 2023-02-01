66e5f38da1ba4b7af9ef8510e9d5a427ac291db9
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-01 14:51:47 +0100

    feat: reverse order on getAllPosts

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -1,68 +1,68 @@
-import { readdirSync, readFileSync } from 'fs';
-import { join } from 'path';
-import matter from 'gray-matter';
+import { readdirSync, readFileSync } from 'fs'
+import { join } from 'path'
+import matter from 'gray-matter'
 
-const postsDirectory = join(process.cwd(), '_posts');
+const postsDirectory = join(process.cwd(), '_posts')
 
 export function getPostSlugs() {
-    return readdirSync(postsDirectory);
+  return readdirSync(postsDirectory)
 }
 
 export function getPostBySlug(slug: string, fields: string[] = []) {
-    const realSlug = slug.replace(/\.md$/, '');
-    const filePath = join(postsDirectory, `${realSlug}.md`);
-    const fileContents = readFileSync(filePath, 'utf8');
-    const { data, content } = matter(fileContents);
+  const realSlug = slug.replace(/\.md$/, '')
+  const filePath = join(postsDirectory, `${realSlug}.md`)
+  const fileContents = readFileSync(filePath, 'utf8')
+  const { data, content } = matter(fileContents)
 
-    type Items = {
-        [key: string]: string;
+  type Items = {
+    [key: string]: string
+  }
+  const items: Items = {}
+
+  fields.forEach((field) => {
+    if (field === 'slug') {
+      items[field] = realSlug
     }
-    const items: Items = {}
+    if (field === 'content') {
+      items[field] = content
+    }
+    if (typeof data[field] !== 'undefined') {
+      items[field] = data[field]
+    }
+  })
 
-    fields.forEach(field => {
-        if (field === 'slug') {
-            items[field] = realSlug;
-        }
-        if (field === 'content') {
-            items[field] = content;
-        }
-        if (typeof data[field] !== 'undefined') {
-            items[field] = data[field];
-        }
-    })
-
-    return items;
+  return items
 }
 
 export function getAllPosts(fields: string[] = []) {
-    const slugs = getPostSlugs();
-    const posts = slugs
-        .map(slug => getPostBySlug(slug, fields))
-        .sort((post1, post2) => (post1.publicationDate > post2.publicationDate ? -1 : 1))
-    ;
-
-    return posts;
+  const slugs = getPostSlugs()
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    .sort((post1, post2) =>
+      post1.publicationDate > post2.publicationDate ? 1 : -1,
+    )
+  return posts
 }
 
 export function getLatestPosts(n: number, fields: string[] = []) {
-    const slugs = getPostSlugs();
-    const posts = slugs
-        .map(slug => getPostBySlug(slug, fields))
-        .sort((post1, post2) => (post1.publicationDate > post2.publicationDate ? -1 : 1))
-        .slice(0, n)
-    ;
-
-    return posts;
+  const slugs = getPostSlugs()
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    .sort((post1, post2) =>
+      post1.publicationDate > post2.publicationDate ? -1 : 1,
+    )
+    .slice(0, n)
+  return posts
 }
 
 export function getRandomPost(excludedSlug?: string, fields: string[] = []) {
-    let slugs = getPostSlugs();
-    if (excludedSlug) {
-        slugs = slugs.filter(slug => slug !== `${excludedSlug}.md`);
-    }
+  let slugs = getPostSlugs()
+  if (excludedSlug) {
+    slugs = slugs.filter((slug) => slug !== `${excludedSlug}.md`)
+  }
 
-    const randomSlug = slugs[Math.floor(Math.random()*slugs.length)]
-    const post = getPostBySlug(randomSlug, fields);
+  const randomSlug = slugs[Math.floor(Math.random() * slugs.length)]
+  const post = getPostBySlug(randomSlug, fields)
 
-    return post;
+  return post
 }
