365af45f7c73aae517886e20a4dde4c41efb8621
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-02 13:39:32 +0100

    fix: add SSRProvider for react-aria

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
+import { SSRProvider } from 'react-aria'
 import { lightTheme, darkTheme } from '../styles/theme'
 import Head from '../components/head'
 import Layout from '../components/layout'
@@ -8,23 +9,25 @@ import { ContactModalProvider } from '../contexts/ContactModalProvider'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextThemesProvider
-      defaultTheme="system"
-      attribute="class"
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className,
-      }}
-    >
-      <NextUIProvider>
-        <ContactModalProvider>
-          <Head />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ContactModalProvider>
-      </NextUIProvider>
-    </NextThemesProvider>
+    <SSRProvider>
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <ContactModalProvider>
+            <Head />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </ContactModalProvider>
+        </NextUIProvider>
+      </NextThemesProvider>
+    </SSRProvider>
   )
 }
 
