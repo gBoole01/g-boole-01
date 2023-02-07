189cce8e0939ba6468581926ed2a7dd487fde358
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-07 10:25:04 +0100

    refactor: remove tests from v1

--- __tests__/Intro.test.jsx
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest'
-import { render, screen, within } from '@testing-library/react'
-import Intro from '../src/components/intro'
-
-test('Intro', () => {
-    render(<Intro />)
-    const heading = within(screen.getByRole('heading')).getByText('Qui suis-je ?')
-    expect(heading).toBeDefined()
-})
