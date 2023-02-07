7b894fb087fbf70d226125deb02fa580ac5351b7
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-02-07 09:41:03 +0100

    feat: add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,27 @@ import type { AppProps } from 'next/app'
 import { NextUIProvider } from '@nextui-org/react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { SSRProvider } from 'react-aria'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 import { lightTheme, darkTheme } from '../styles/theme'
 import Head from '../components/head'
 import Layout from '../components/layout'
+import * as gtag from '../lib/gtag'
 import { ContactModalProvider } from '../contexts/ContactModalProvider'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <SSRProvider>
       <NextThemesProvider
